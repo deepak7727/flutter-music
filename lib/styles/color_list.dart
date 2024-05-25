45de879c9730e57e0809ab9553a8717789ed0de1
Deepak <dp245289@gmail.com>
2024-05-25 13:38:43 +0530

    first init

---
+++ lib/styles/color_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ColorList{
+  // Primary Blue Colors
+  static const Color black = Color(0xFF000000); // Blue 500
+  static const Color white = Color(0xFFFFFFFF); // Blue 500
+  
+  
+  static const Color parimayColor = Color(0xFF2196F3); // Blue 500
+  static const Color scarfoldBackgroundColor = Color(0xFFBBDEFB); // Blue 100
+ static const Color success = Color(0xFF4CAF50); // Green 500
+  static const Color danger = Color(0xFFE53935); // Red 600
+
+  // Accent Blue Colors
+
+
+  static const Color tileBackgroundColor = Color(0xFF808080); // Blue A200
+
+
+  static const Color accentBlue = Color(0xFF448AFF); // Blue A200
+  static const Color accentLightBlue = Color(0xFF82B1FF); // Blue A100
+  static const Color accentDarkBlue = Color(0xFF2979FF); // Blue A400
+
+  // Additional Blue Shades
+  static const Color blueGrey = Color(0xFF607D8B); // Blue Grey 500
+  static const Color lightBlueGrey = Color(0xFFCFD8DC); // Blue Grey 100
+  static const Color darkBlueGrey = Color(0xFF455A64); // Blue Grey 700
+}
